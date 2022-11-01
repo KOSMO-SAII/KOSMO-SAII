b3939f5dd1dfb0aa6eeba719ce810918f65a25cc
Hewn05 <90302651+Hewn05@users.noreply.github.com>
2022-11-01 10:03:48 +0900

    courseDAO insertCourse

--- SAII/src/main/java/saii/controller/courseWritePageController.java
+++ SAII/src/main/java/saii/controller/courseWritePageController.java
@@ -29,43 +29,6 @@ public class courseWritePageController extends HttpServlet{
 		courseDTO cdto ;
 		int len = 0;
 		
-		if(req.getParameter("c1") != null) {
-			cdto = cdao.toCDTO(1, req.getParameter("c1"));
-			req.setAttribute("c1", cdto);
-			len = 1;
-		} 
-		if(req.getParameter("c2") != null) {
-			cdto = cdao.toCDTO(2, req.getParameter("c2"));
-			req.setAttribute("c2", cdto);
-			len = 2;
-		} 
-		if(req.getParameter("c3") != null) {
-			cdto = cdao.toCDTO(3, req.getParameter("c3"));
-			req.setAttribute("c3", cdto);
-			len = 3;
-		} 
-		if(req.getParameter("c4") != null) {
-			cdto = cdao.toCDTO(4, req.getParameter("c4"));
-			req.setAttribute("c4", cdto);
-			len = 4;
-		}
-		if(req.getParameter("c5") != null) {
-			cdto = cdao.toCDTO(5, req.getParameter("c5"));
-			req.setAttribute("c5", cdto);
-			len = 5;
-		}
-		if(req.getParameter("c6") != null) {
-			cdto = cdao.toCDTO(6, req.getParameter("c6"));
-			req.setAttribute("c6", cdto);
-			len = 6;
-		}
-		if(req.getParameter("c7") != null) {
-			cdto = cdao.toCDTO(7, req.getParameter("c7"));
-			req.setAttribute("c7", cdto);
-			len = 7;
-		}
-		System.out.println("blabla");
-		//resp.sendRedirect("http://localhost:8081/SAII/saii/my/test.jsp");
 		req.getRequestDispatcher("saii/test.jsp").forward(req, resp);
 	}
 	
