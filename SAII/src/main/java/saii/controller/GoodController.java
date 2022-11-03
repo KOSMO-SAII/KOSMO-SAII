4c3ec56032d434be91f2e007c5873c1d750b6a43
soil-rabbit <59332378+soil-rabbit@users.noreply.github.com>
2022-11-03 16:54:04 +0900

    fix
    좋아요~

--- SAII/src/main/java/saii/controller/GoodController.java
+++ SAII/src/main/java/saii/controller/GoodController.java
@@ -8,72 +8,40 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.json.simple.JSONObject;
+
 import saii.domain.goodDAO;
 import saii.domain.mainboardDAO;
-import saii.domain.memberDAO;
-import saii.dto.goodDTO;
 import saii.dto.mainboardDTO;
-import saii.dto.memberDTO;
 
 @WebServlet("/good")
 public class GoodController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException{
-		if(Integer.parseInt(req.getParameter("mode")) == 0) {
-			String m_id = req.getParameter("m_id");
-			
-			memberDTO memdto = new memberDTO();
-			memberDAO memdao = new memberDAO();
-			String id = req.getSession().getAttribute("UserId").toString();
-			memdto = memdao.userinfo(id);
-			
-			goodDAO gdao = new goodDAO();
-			gdao.pushGood(m_id, memdto.getNickname());
-			
-			memdao.close();
-			gdao.close();
-		}
-		if(Integer.parseInt(req.getParameter("mode")) == 1) {
-			String m_id = req.getParameter("m_id");
-			
-			memberDTO memdto = new memberDTO();
-			memberDAO memdao = new memberDAO();
-			String id = req.getSession().getAttribute("UserId").toString();
-			memdto = memdao.userinfo(id);
-			
-			goodDAO gdao = new goodDAO();
-			gdao.cancelGood(m_id, memdto.getNickname());
-			
-			memdao.close();
-			gdao.close();
-		}
-		
-		// 게시글 가져오기
-		mainboardDAO dao = new mainboardDAO();
+		String text_condition = req.getParameter("text_condition");
 		String m_id = req.getParameter("m_id");
-		mainboardDTO dto = dao.selectView(m_id);
-								
-		//dto.setContent(dto.getContent().replaceAll("/r/n", "<br/>"));
-								
-		memberDTO memdto = new memberDTO();		
-		memberDAO memdao = new memberDAO();
-		String id = req.getSession().getAttribute("UserId").toString();
-		memdto = memdao.userinfo(id);
-					
-		goodDTO gdto = new goodDTO();
-					
+		String nickname = req.getParameter("nickname");
+		
+		JSONObject jobj = new JSONObject();
 		goodDAO gdao = new goodDAO();
-		//boolean goodWhether = gdao.goodWhether(dto.getM_id(), memdto.getNickname());
-					
-		memdao.close();
+		
+		if(text_condition.equals("하트아님")) {
+			gdao.pushGood(m_id, nickname);
+			jobj.put("heart", "noheart");
+		}else {
+			gdao.cancelGood(m_id, nickname);
+			jobj.put("heart", "yesheart");
+		}
+
+		mainboardDAO mdao = new mainboardDAO();
+		mainboardDTO mdto = mdao.selectView(m_id);
+		
+		jobj.put("goodcount", mdto.getGoodcount());
+		
 		gdao.close();
-					
-		//req.setAttribute("goodWhether", goodWhether);
-		req.setAttribute("memdto", memdto);
-								
-		dao.close();
-				
-		req.setAttribute("dto", dto);
-		req.getRequestDispatcher("/saii/MainView.jsp").forward(req, resp);
+		mdao.close();
+		
+		resp.setContentType("application/x-json; charset=utf-8");
+		resp.getWriter().print(jobj);
 	}
 }
