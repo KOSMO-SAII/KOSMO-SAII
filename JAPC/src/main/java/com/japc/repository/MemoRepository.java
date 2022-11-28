af5b89d16df0df05c635d14c8bfcc1099174c6bb
Hewn05 <90302651+Hewn05@users.noreply.github.com>
2022-11-28 15:46:06 +0900

    1128

--- JAPC/src/main/java/com/japc/repository/MemoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.japc.repository;
-
-import com.japc.entity.Memo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemoRepository extends JpaRepository<Memo, Integer> {
-
-}
