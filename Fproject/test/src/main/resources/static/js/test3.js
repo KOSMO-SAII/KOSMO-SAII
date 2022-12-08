929af832d4068d28f4069609f37e8b416daae0da
Hewn05 <90302651+Hewn05@users.noreply.github.com>
2022-12-08 19:16:46 +0900

    1208

--- Fproject/test/src/main/resources/static/js/test3.js
+++ Fproject/test/src/main/resources/static/js/test3.js
@@ -1,64 +1,61 @@
 src="https://code.jquery.com/jquery-3.6.1.js"; integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="; crossorigin="anonymous";
 //console.log("실행");
-var id;
+var id = ""
+var c = ""
+
 $(".btn").click(function(event){
 var e = event.target
 //console.log("클릭")
 //console.dir(e)
 //console.dir(e.parentElement.id)
 //console.dir(e.parentElement.localName=='li')
+if(e.parentElement.id != ""){
  id = e.parentElement.id
- //getInfo(id);
- getminfo();
+ }
+if(e.parentElement.className != ""){
+ c = e.parentElement.className
+ }
+
+ console.log(e + '    ' +e.parentElement + '   ' + id + '   ' +  c)
+ var ul =$('.flnon')
+ ul.empty()
+ getInfo(id, c);
 })
 
+$("btn_all_active").click(function(event){
+    id = ""
+    c = ""
+})
 
-function getInfo(id){
-    var info;
-    var infoitem=[];
-    fetch('https://apis.data.go.kr/B551011/KorService/areaBasedList?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&&pageNo=1&numOfRows=10&MobileApp=AppTest&MobileOS=ETC&arrange=A&areaCode='+id+'&_type=json')
-    .then((response) => response.json())
-    .then((data) => {
-        info = data;
-        infoitem=data.response.body.items.item
-        //console.log(info);
-       console.log(infoitem)
-
-
-
+$(".js_multi").child.click(function(event){
+    var e = event.target
+    id = e.id
+    getInfo(id,'')
     })
 
-}
-function getminfo(){
+function getInfo(id, c){
     var info;
     var infoitem=[];
-    fetch('https://apis.data.go.kr/B551011/KorService/searchFestival?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&numOfRows=10&pageNo=12&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C&eventStartDate=20220101&eventEndDate=20221231')
+    var param = ('&areaCode='+id+'&eventStartDate=2022'+c+'01&eventEndDate=2022'+c+'31')
+    console.log(param);
+    fetch('https://apis.data.go.kr/B551011/KorService/searchFestival?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C'+param)
     .then((response) => response.json())
     .then((data) => {
         info = data;
         infoitem=data.response.body.items.item
-        //console.log(info);
-       console.log(info)
-    for(var j=0;j<infoitem.length;j++){
+        console.log(infoitem)
+   if(true){
+        for(var j=0;j<infoitem.length;j++){
             printInfo(infoitem[j])
-            console.log("반복")
         }
+      }
     })
 
 }
+
 function printInfo(infoitem){
-    console.log("반복2")
     var ul =$('.flnon');
     var li=document.createElement('li');
     li.innerHTML = '<image class="image" src="'+infoitem.firstimage+'"></image><p class="title">'+infoitem.title+'</p>';
     ul[0].appendChild(li);
 }
-
-const common={
-    totalCount :1196,
-    recordPerPage:10
-    nacPage:12
-};
-$('document').ready(function(){
-setPageInfo(common.recordsPerPage.common.navPage,1)
-})
