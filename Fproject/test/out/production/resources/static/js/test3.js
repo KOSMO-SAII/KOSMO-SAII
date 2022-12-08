c64c238749b5a0582f9a0e4d17fea8889b67fb39
yejinee716 <115680841+yejinee716@users.noreply.github.com>
2022-12-08 19:06:42 +0900

    ss
    ss

--- Fproject/test/out/production/resources/static/js/test3.js
+++ Fproject/test/out/production/resources/static/js/test3.js
@@ -8,7 +8,8 @@ var e = event.target
 //console.dir(e.parentElement.id)
 //console.dir(e.parentElement.localName=='li')
  id = e.parentElement.id
- getInfo(id);
+ //getInfo(id);
+ getminfo();
 })
 
 
@@ -22,34 +23,29 @@ function getInfo(id){
         infoitem=data.response.body.items.item
         //console.log(info);
        console.log(infoitem)
-   if(){}
-        for(var j=0;j<infoitem.length;j++){
 
-              ...
-                printInfo(infoitem[j])
-                console.log("월")
-        }
-      }
+
+
     })
 
 }
-//function getminfo(class){
-//    var info;
-//    var infoitem=[];
-//    fetch('https://apis.data.go.kr/B551011/KorService/searchFestival?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&numOfRows=10&pageNo=12&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C&eventStartDate=20220101&eventEndDate=20221231')
-//    .then((response) => response.json())
-//    .then((data) => {
-//        info = data;
-//        infoitem=data.response.body.items.item
-//        //console.log(info);
-//       console.log(infoitem)
-//    for(var j=0;j<infoitem.length;j++){
-//            printInfo(infoitem[j])
-//            console.log("반복")
-//        }
-//    })
-//
-//}
+function getminfo(){
+    var info;
+    var infoitem=[];
+    fetch('https://apis.data.go.kr/B551011/KorService/searchFestival?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&numOfRows=10&pageNo=12&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C&eventStartDate=20220101&eventEndDate=20221231')
+    .then((response) => response.json())
+    .then((data) => {
+        info = data;
+        infoitem=data.response.body.items.item
+        //console.log(info);
+       console.log(info)
+    for(var j=0;j<infoitem.length;j++){
+            printInfo(infoitem[j])
+            console.log("반복")
+        }
+    })
+
+}
 function printInfo(infoitem){
     console.log("반복2")
     var ul =$('.flnon');
