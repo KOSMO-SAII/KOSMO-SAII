3968e0b5b7bfd29d234ce51a6afc363b51d0df2c
jsj9877 <115458060+jsj9877@users.noreply.github.com>
2022-12-12 20:15:26 +0900

    22

--- Fproject/test/src/main/java/com/example/test/controller/IndexController.java
+++ Fproject/test/src/main/java/com/example/test/controller/IndexController.java
@@ -6,12 +6,12 @@
 //import org.springframework.stereotype.Controller;
 //import org.springframework.ui.Model;
 //import org.springframework.web.bind.annotation.GetMapping;
-
+//
 //@RequiredArgsConstructor
 //@Controller
 //public class IndexController {//페이지에 관련된 컨트롤러
 //    private final ReviewCourseService reviewCourseService;
-
+//
 //    @GetMapping("/index")
 //    public String index(Model model){
 //        model.addAttribute("courseReview", reviewCourseService.findAllDesc());
