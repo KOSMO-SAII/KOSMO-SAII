6bb2437850531a567d2fdb2bc7a79038e54399f9
seonmeelee <115458063+seonmeelee@users.noreply.github.com>
2022-12-13 17:25:24 +0900

    123

--- Fproject/test/src/main/java/com/example/test/domain/MemberDTO.java
+++ Fproject/test/src/main/java/com/example/test/domain/MemberDTO.java
@@ -5,41 +5,43 @@ import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.util.Date;
 
 @Getter
 @Setter
 public class MemberDTO {
 
-    @NotEmpty(message = "주소를 입력해주세요.")
+    @NotEmpty(message = "주소는 필수 입력 값입니다.")
     private String address;
 
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date birthday;
 
-    @NotEmpty(message = "이메일을 입력해주세요.")
-    @Email(message = "이메일 형식으로 입력해주세요.")
+    @NotEmpty(message = "이메일은 필수 입력 값입니다.")
+    @Email(message = "@을 포함하여 이메일 형식으로 입력해주세요.")
     private String email;
 
     private String gender;
 
-    @NotEmpty(message = "아이디를 입력해주세요.")
-    @Pattern(regexp="[a-zA-Z]+")
+    @NotBlank(message = "아이디는 필수 입력 값입니다.")
+    @Pattern(regexp="/[a-zA-Z0-9]/")
+    @Size(min=6, max=15, message = "아이디는 영어와 숫자 6~15자 이하로 입력해주세요")
     private String loginId;
 
-    @NotEmpty(message = "비밀번호를 입력해주세요.")
-    @Length(min = 8, max=16, message="비밀번호는 8자이상, 16자 이하로 입력해주세요")
+    @NotEmpty(message = "비밀번호는 필수 입력 값입니다.")
+    @Size(min=8, max=16, message = "비밀번호는 숫자, 특수문자가 각각 최소 1개이상을 포함한 8~16자 이하로 입력해주세요")
+    @Pattern(regexp = "/^(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,16}$/")
     private String loginPw;
 
     private String nProfileImg;
 
-    @NotEmpty(message = "이름을 입력해주세요")
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    @Size(min=2, message = "이름은 2자 이상으로 입력해주세요")
     private String name;
 
-    @NotEmpty(message = "닉네임을 입력해주세요")
+    @NotEmpty(message = "닉네임은 필수 입력 값입니다.")
+    @Size(min=2, max=10, message = "닉네임은 2~10자 이하로 입력해주세요")
     private String nickname;
 
     private String oProfileImg;
