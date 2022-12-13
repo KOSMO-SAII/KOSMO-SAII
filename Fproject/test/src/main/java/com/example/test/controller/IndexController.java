c08c48197807cbf83b83abe72c34cba2e52ec413
jsj9877 <115458060+jsj9877@users.noreply.github.com>
2022-12-13 10:59:21 +0900

    1212

--- Fproject/test/src/main/java/com/example/test/controller/IndexController.java
+++ Fproject/test/src/main/java/com/example/test/controller/IndexController.java
@@ -1,21 +1,25 @@
-//package com.example.test.controller;
-//
-//import com.example.test.entity.CourseReview;
-//import com.example.test.service.ReviewCourseService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//@RequiredArgsConstructor
-//@Controller
-//public class IndexController {//페이지에 관련된 컨트롤러
-//    private final ReviewCourseService reviewCourseService;
-//
-//    @GetMapping("/index")
-//    public String index(Model model){
-//        model.addAttribute("courseReview", reviewCourseService.findAllDesc());
-//        return "index";
-//        //
-//    }
-//}
+package com.example.test.controller;
+
+import com.example.test.service.MemberService;
+import com.example.test.service.ReviewCourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@Controller
+public class IndexController {//페이지에 관련된 컨트롤러
+    public final ReviewCourseService reviewCourseService;
+
+    @GetMapping("/index")
+    public String index(Model model, Principal principal){
+        model.addAttribute("courseReview", reviewCourseService.findAllDesc());
+        if(principal != null){
+            model.addAttribute("name",principal.getName());
+        }
+        return "index";
+    }
+}
--- Fproject/test/src/main/java/com/example/test/controller/ReviewCourseController.java
+++ Fproject/test/src/main/java/com/example/test/controller/ReviewCourseController.java
@@ -1,22 +1,29 @@
-//package com.example.test.controller;
-//
-//import com.example.test.service.ReviewCourseService;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@RequiredArgsConstructor
-//@Controller
-//public class ReviewCourseController {
-//
-//    public final ReviewCourseService reviewCourseService;
-//
-////    @RequestMapping("/reviewCourse")
-////    public  String index(Model model){
-////        model.addAttribute("courseReview",reviewCourseService.findAllDesc());
-////        return "review/reviewCourse";
-////    }
-//
-//}
-//
+package com.example.test.controller;
+
+import com.example.test.service.ReviewCourseService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.security.Principal;
+
+@RequiredArgsConstructor
+@Controller
+public class ReviewCourseController {
+
+    public final ReviewCourseService reviewCourseService;
+
+    @RequestMapping("/reviewCourse")
+    public  String index(Model model, Principal principal){
+        model.addAttribute("courseReview",reviewCourseService.findAllDesc());
+        if(principal != null){
+            model.addAttribute("name",principal.getName());
+        }
+        return "review/reviewCourse";
+    }
+
+
+
+}
+
