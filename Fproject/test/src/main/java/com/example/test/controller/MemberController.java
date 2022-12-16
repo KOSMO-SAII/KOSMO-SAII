17b4db723d5835c0bde2b37de978f7ed5a6df982
vodtndl <115458013+vodtndl@users.noreply.github.com>
2022-12-16 17:57:45 +0900

    signup

--- Fproject/test/src/main/java/com/example/test/controller/MemberController.java
+++ Fproject/test/src/main/java/com/example/test/controller/MemberController.java
@@ -175,6 +175,29 @@ public class MemberController {
         }
     }
 
+    @RequestMapping(value = "/jusoPopup")
+    public ModelAndView jusoPopup(HttpServletRequest request, @RequestParam HashMap<String, String> p, Locale locale) {
+
+        // callback 함수가 실행되어야하니 호출한 html 파일로 return
+        ModelAndView mav = new ModelAndView("/mypage/memberUpdate.html");
+
+        String inputYn = request.getParameter("inputYn");
+        String zipNo = request.getParameter("zipNo");
+        String roadAddrPart1 = request.getParameter("roadAddrPart1");
+        String roadAddrPart2 = request.getParameter("roadAddrPart2");
+        String addrDetail = request.getParameter("addrDetail");
+        String jibunAddr = request.getParameter("jibunAddr");
+
+        mav.addObject("inputYn", inputYn);
+        mav.addObject("zipNo", zipNo);
+        mav.addObject("roadAddrPart1", roadAddrPart1);
+        mav.addObject("roadAddrPart2", roadAddrPart2);
+        mav.addObject("jibunAddr", jibunAddr);
+        mav.addObject("addrDetail", addrDetail);
+
+        return mav;
+    }
+
 
 
 
