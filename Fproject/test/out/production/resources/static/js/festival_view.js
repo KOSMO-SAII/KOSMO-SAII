31c0c979541195606c67b823c65af3b01674624b
yejinee716 <115680841+yejinee716@users.noreply.github.com>
2022-12-16 17:13:06 +0900

    dd
    dd

--- Fproject/test/out/production/resources/static/js/festival_view.js
+++ Fproject/test/out/production/resources/static/js/festival_view.js
@@ -1,7 +1,7 @@
 src="https://code.jquery.com/jquery-3.6.1.js"; integrity="sha256-3zlB5s2uwoUzrXK3BT7AX3FyvojsraNFxCc2vC/7pNI="; crossorigin="anonymous";
 console.log("안녕")
-getInfo();
-  function getInfo(){
+galleryGo();
+  function galleryGo(){
       var info;
       var infoitem=[];
       var contentTypeId=15;
@@ -11,6 +11,7 @@ getInfo();
       .then((data) => {
             infoitem=data.response.body.items.item[0]
             console.log(infoitem)
+            printImg(infoitem)
             printInfo(infoitem)
          })
 //   if(true){
@@ -21,7 +22,7 @@ getInfo();
 //      })
   }
 
-  function printInfo(infoitem){
+  function printImg(infoitem){
   console.log(infoitem.firstimage)
   console.log(infoitem.title)
 
@@ -31,3 +32,37 @@ getInfo();
       img.setAttribute("height", "600");
     $('.swiper-container')[0].prepend(img)
   }
+
+  function printInfo(infoitem){
+    console.log(infoitem.overview)
+    console.log(infoitem.title)
+
+      var detail = document.createElement("DIV");
+      detail.setAttribute("class", infoitem.overview);
+      detail.innerHTML='<p>'+infoitem.overview+'</p>'
+      $('.inr_wrap')[0].append(detail)
+    }
+
+   koreaservice();
+     function koreaservice(){
+         var info;
+         var infoitem=[];
+         var contentId=id
+         fetch('https://apis.data.go.kr/B551011/KorService/detailIntro?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&MobileOS=ETC&MobileApp=AppTest&_type=json&contentId='+contentId+'&contentTypeId=15')
+         .then((response) => response.json())
+         .then((data) => {
+               infoitem=data.response.body.items.item[0]
+               console.log(infoitem)
+         if(true){
+               for(var j=0;j<infoitem.length;j++){
+               printInfo(infoitem[j])
+               }
+            }
+         })
+     }
+     function printservice(infoitem){
+     var div=$('.inr_wrap');
+     var p=document.createElement('p');
+     p.innerHTML='<p>'+infoitem[j]+'</p>'
+     }
+
