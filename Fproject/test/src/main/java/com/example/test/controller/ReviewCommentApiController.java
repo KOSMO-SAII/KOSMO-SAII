6e9ffc880ddce5b62f21601b6c976399aad299cc
jsj9877 <115458060+jsj9877@users.noreply.github.com>
2022-12-22 10:57:08 +0900

    1212

--- Fproject/test/src/main/java/com/example/test/controller/ReviewCommentApiController.java
+++ Fproject/test/src/main/java/com/example/test/controller/ReviewCommentApiController.java
@@ -3,10 +3,13 @@ package com.example.test.controller;
 import com.example.test.config.LoginUser;
 import com.example.test.domain.MemberDTO;
 import com.example.test.domain.ReviewCommentRequestDTO;
+import com.example.test.service.MemberService;
 import com.example.test.service.ReviewCommentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RequestMapping("/api")
@@ -14,10 +17,12 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewCommentApiController {
 
     private final ReviewCommentService reviewCommentService;
+    private final MemberService memberService;
 
     @PostMapping("/reviews/{id}/comments")
     public ResponseEntity commentSave(@PathVariable Long id, @RequestBody ReviewCommentRequestDTO dto,
-                                      @LoginUser MemberDTO memberDTO){
+                                               @LoginUser MemberDTO memberDTO){
+        System.out.println(memberService.getMember().getNickname());
         return ResponseEntity.ok(reviewCommentService.commentSave(memberDTO.getNickname(), id, dto));
     }
 }
