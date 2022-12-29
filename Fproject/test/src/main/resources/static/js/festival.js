a6d25f29f1e2ec209a62f65c998b102ba01cc6aa
yejinee716 <115680841+yejinee716@users.noreply.github.com>
2022-12-29 18:10:18 +0900

    dd
    dd

--- Fproject/test/src/main/resources/static/js/festival.js
+++ Fproject/test/src/main/resources/static/js/festival.js
@@ -37,6 +37,7 @@ console.log(e.parentElement.parentElement.id)
  getInfo(id, c);
 })
 
+
 $("btn_all_active").click(function(event){
     id = ""
     c = ""
@@ -61,7 +62,7 @@ function getInfo(id, c){
         infoitem=data.response.body.items.item
         console.log(infoitem)
    if(true){
-        for(var j=0;j<infoitem.length;j++){
+        for(var j=0; j<infoitem.length; j++){
             printInfo(infoitem[j])
         }
       }
@@ -69,15 +70,16 @@ function getInfo(id, c){
 
 }
 
+
+
 function printInfo(infoitem){
     var ul =$('.flnon');
     var li=document.createElement('li');
      var eventstartdate = infoitem.eventstartdate.substr(0,4) + '-' + infoitem.eventstartdate.substr(4,2) + '-' + infoitem.eventstartdate.substr(6,2)
-     var eventenddate = infoitem.eventstartdate.substr(0,4) + '-' + infoitem.eventstartdate.substr(4,2) + '-' + infoitem.eventstartdate.substr(6,2)
+     var eventenddate = infoitem.eventenddate.substr(0,4) + '-' + infoitem.eventenddate.substr(4,2) + '-' + infoitem.eventenddate.substr(6,2)
     li.innerHTML = '<div class="imgandtext"><a href="/festivalview/'+infoitem.contentid+'"><image class="image" src="'+infoitem.firstimage+'" alt="이미지 준비중입니다."></image></a><div class="titleanddate"><a href="/festivalview/'+infoitem.contentid+'"><p class="title">'+infoitem.title+'</p><p class="date">['+''+eventstartdate+'~'+eventenddate+']</p></div></a></div>';
     ul[0].appendChild(li);
 }
-
 function allInfo(){
   var infoitem=[];
 fetch('https://apis.data.go.kr/B551011/KorService/searchFestival?serviceKey=%2Bj0evNiGTyeurclaWudJiAx8TTZR7CIDuaVb7eKSqMRM8cgCFe%2BRjhZUNBZubBIRZhlHxVvK63mnQwy53w%2Bqxg%3D%3D&numOfRows=10&pageNo=10&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=C&eventStartDate=20220101&eventEndDate=20221231')
@@ -96,10 +98,12 @@ function printAll(infoitem){
     var ul =$('.tag_list js_multi');
     var li=document.createElement('li');
      var eventstartdate = infoitem.eventstartdate.substr(0,4) + '-' + infoitem.eventstartdate.substr(4,2) + '-' + infoitem.eventstartdate.substr(6,2)
-     var eventenddate = infoitem.eventstartdate.substr(0,4) + '-' + infoitem.eventstartdate.substr(4,2) + '-' + infoitem.eventstartdate.substr(6,2)
+     var eventenddate = infoitem.eventenddate.substr(0,4) + '-' + infoitem.eventenddate.substr(4,2) + '-' + infoitem.eventenddate.substr(6,2)
     li.innerHTML = '<div class="imgandtext"><a href="/festivalview/'+infoitem.contentid+'"><image class="image" src="'+infoitem.firstimage+'" alt="이미지 준비중입니다."></image></a><div class="titleanddate"><a href="/festivalview/'+infoitem.contentid+'"><p class="title">'+infoitem.title+'</p><p class="date">['+''+eventstartdate+'~'+eventenddate+']</p></div></a></div>';
-    ul[0].appendChild(li);
+    ul.appendChild(li);
 }
+
+
 
 //var All=false;
 //function SelectAll(){
