204fe947cd7861fdf96ecfc296ecb841026d766f
seonmeelee <115458063+seonmeelee@users.noreply.github.com>
2022-12-30 16:06:38 +0900

    123

--- Fproject/test/src/main/java/com/example/test/controller/MemberController.java
+++ Fproject/test/src/main/java/com/example/test/controller/MemberController.java
@@ -77,6 +77,11 @@ MemberController {
         return "redirect:/";
     }
 
+    @GetMapping("/use")
+    public String useTo() {
+        return "/howtoUse";
+    }
+
     @GetMapping("/login")
     public String loginMember() {
 
